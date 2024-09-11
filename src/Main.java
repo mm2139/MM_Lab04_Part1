de1c25d6237ef83b7f0fc7e0b92589d64d337788
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-11 08:35:50 -0400

    finished lab 4 part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int intOperandA = 4;
+        int intOperandB = 7;
+        int intSum;
+        int intDifference;
+        int intProduct;
+        int intQuotient;
+        int intModulo;
+
+        double doubleOperandA = 4;
+        double doubleOperandB = 7;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleModulo;
+
+        intSum = intOperandA + intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        System.out.println("The sum, difference, product, quotient, and modulo of " + intOperandA + " and " + intOperandB + " are " + intSum + ", " + intDifference + ", " + intProduct + ", " + intQuotient + ", and " + intModulo + ".");
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        doubleModulo= doubleOperandA % doubleOperandB;
+
+        System.out.println("The sum, difference, product, quotient, and modulo of " + doubleOperandA + " and " + doubleOperandB + " are " + doubleSum+", "+ doubleDifference + ", " + doubleProduct + ", " + doubleQuotient+ ", and " + doubleModulo + ".");
+
+
     }
 }
